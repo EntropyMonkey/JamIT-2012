b0b3eef944071047a398750f0a5a13f75f0c7463
PeterOe <peter.oe@gmail.com>
2012-10-06 19:03:39 +0200

    Added level generation

---
+++ UnityProject/Assets/scripts/LevelGenerator.cs
@@ -0,0 +1,67 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+class LevelGenerator : MonoBehaviour
+{
+    public Camera GameCamera;
+    public GameObject prefab;
+    private Vector3 StartPos;
+    private Vector3 EndPos;
+    public const float size = 1.85F;  //GO size = size * 2
+    void Start()
+    {
+        StartPos = GameCamera.transform.position;
+        EndPos = StartPos;
+        EndPos += new Vector3(0, -2, 0);
+        EndPos.Scale(new Vector3(1, 1, 0));
+        
+        for (int i = 0; i < 4; i++)
+        {   
+            GenerateNormalScretch(EndPos);
+            EndPos += new Vector3(size*2, 0, 0);
+        }
+    }
+
+    void Update()
+    {
+        if(GameCamera.transform.position.x +4 > EndPos.x)
+        {
+            int r = Random.Range(0, 2);
+            if (r == 0)
+            {
+                GenerateNormalScretch(EndPos);
+                EndPos += new Vector3(size*2, 0, 0);
+                GenerateNormalScretch(EndPos);
+                EndPos += new Vector3(size * 2, 0, 0);
+            }
+            else if (r == 1)
+            {
+                GenerateNormalScretch(EndPos);
+                EndPos += new Vector3(size * 4, 0, 0);
+                GenerateNormalScretch(EndPos);
+                EndPos += new Vector3(size * 2, 0, 0);
+            }
+            else if (r == 2)
+            {
+                GenerateNormalScretch(EndPos);
+                EndPos += new Vector3(size * 3, 0, 0);
+                GenerateNormalScretch(EndPos);
+                EndPos += new Vector3(size * 3, 0, 0);
+            }
+        }
+    }
+
+    private void GenerateNormalScretch(Vector3 pos)
+    {
+        GameObject go = Instantiate(prefab) as GameObject;
+        go.transform.position = pos;
+    }
+
+    private void GenerateDoubleScretch()
+    {
+    }
+
+    private void GenerateJump()
+    {
+    }
+}
